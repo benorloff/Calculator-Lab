39dc05dee45bdb28aeb2324528fb8d0fe23087b3
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-03-11 16:07:58 -0800

    Declared buttons in JS and started rudimentary styling in CSS

---
+++ js/app.js
@@ -0,0 +1,49 @@
+
+
+
+// Declare the buttons
+const btn = {
+    CE: document.getElementById('btnCE'),
+    Divide: document.getElementById('btnDivide'),
+    Multiply: document.getElementById('btnMultiply'),
+    Seven: document.getElementById('btnSeven'),
+    Eight: document.getElementById('btnEight'),
+    Nine: document.getElementById('btnNine'),
+    C: document.getElementById('btnC'),
+    Four: document.getElementById('btnFour'),
+    Five: document.getElementById('btnFive'),
+    Six: document.getElementById('btnSix'),
+    Minus: document.getElementById('btnMinus'),
+    One: document.getElementById('btnOne'),
+    Two: document.getElementById('btnTwo'),
+    Three: document.getElementById('btnThree'),
+    Plus: document.getElementById('btnPlus'),
+    Zero: document.getElementById('btnZero'),
+    Dot: document.getElementById('btnDot'),
+    Equal: document.getElementById('btnEqual')
+}
+
+// Declare the result span
+const result = document.getElementById('result');
+
+//Set initial result of zero
+result.innerText = 
+
+let output = 0;
+
+console.log(btn);
+
+console.log(btn.Eight.value)
+
+// btn.forEach(el => btn)
+
+
+// const button = document.querySelectorAll('button');
+
+// console.log(button.target.id);
+
+// btn{}.addEventListener('click', function buttonValue(e) {
+//     console.log(e.target.value);
+// });
+
+console.log(output * btn.Eight.value);
