dd6655ace46580e12e26b4e9d84679bcec8f73c2
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-03-11 20:30:14 -0800

    Fixed incorrect valued passed for 7, finished CSS styling. Still have to fix C, CE, and = clicks on zero value.

--- js/app.js
+++ js/app.js
@@ -3,8 +3,6 @@
 
 const button = document.querySelectorAll('button');
 
-const nonInputVals = ['CE', 'C', '=']
-
 // Declare the result span
 const result = document.getElementById('result');
 
@@ -13,18 +11,24 @@ let initialOutput = 0;
 let output = 0;
 result.innerText = initialOutput;
 
-
+// List for button clicks and pass values
 document.addEventListener('click', function buttonClick(e) {
+    // Only pass values if elem clicked is button
     if (e.target.tagName === 'BUTTON') {
+        // Handle first click
         if (result.innerText == initialOutput) {
             output = e.target.value;
             result.innerText = output;
         }  
+        // Subsequent clicks
         else if (result.innerText != initialOutput) {
+            // Don't display non-integer and non-operator values
             if ((e.target.value !== '=') && (e.target.value !== 'C') && (e.target.value !== 'CE')) {
                 result.append(e.target.value);
+            // Clear result with 'C' or 'CE' click
             } else if ((e.target.value === 'C') || (e.target.value === 'CE')) {
                 result.innerText = 0;
+            // Run calculation on equal button click
             } else if (e.target.value === '=') {
                 console.log(eval(result.innerText.valueOf()));
                 result.innerText = (eval(result.innerText.valueOf()));
