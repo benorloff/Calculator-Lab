b4b2c24af137b9836fcfb677c5c8f6edbe44c703
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-03-13 15:23:19 -0700

    Fixed issue with leading zero

--- js/app.js
+++ js/app.js
@@ -28,80 +28,25 @@ document.addEventListener('click', function btnClick(btn) {
     if (btn.target.tagName === 'BUTTON') {
         // Prevent leading zero
         if (subTotal === 0) {
-            subTotal = btn.target.value
+            subTotal = btn.target.value;
+            result.innerText = subTotal;
             console.log('subtotal is 0');
-        } else {
-            switch (true) {
-                case (Number.isInteger(parseInt((btn.target.value)))):
-                case (btn.target.value === '.'):
-                    result.append(btn.target.value);
-                    console.log(`${btn.target.value} is a number`);
-                    break;
-                case (btnArr.opr.includes(btn.target.value)):
-                    result.append(btn.target.value);
-                    console.log(`${btn.target.value} is an operator`);
-                    break;
-                case (btnArr.rst.includes(btn.target.value)):
-                    subTotal = 0;
-                    console.log(`${btn.target.value} is a reset`);
-                    console.log(subTotal);
-                    break;
-                case (btnArr.eql.includes(btn.target.value)):
-                    finalTotal = (eval(subTotal.valueOf()))
-                    console.log(`${btn.target.value} is an equal`);
-                    break;
-            }
+        } else if (btnArr.rst.includes(btn.target.value)) {
+            subTotal = 0;
+            result.innerText = subTotal;
+            console.log(`${btn.target.value} is a reset`);
+            console.log(subTotal);
+        } else if ((Number.isInteger(parseInt((btn.target.value)))) || (btn.target.value === '.')) {
+            result.append(btn.target.value);
+            console.log(`${btn.target.value} is a number`);
+        } else if (btnArr.opr.includes(btn.target.value)) {
+            result.append(btn.target.value);
+            console.log(`${btn.target.value} is an operator`);
+        } else if (btnArr.eql.includes(btn.target.value)) {
+            subTotal = eval(result.innerText.valueOf());
+            result.innerText = subTotal;
+            console.log(subTotal)
+            console.log(`${btn.target.value} is an equal`);
         }
     }
 })
-
-// function btnInput(btn) {
-//     // document.addEventListener('click', function getBtnVal(btn) {
-//         switch (true) {
-//             case (Number.isInteger(parseInt((btn.target.value)))):
-//             case (btn.target.value === '.'):
-//                 console.log(`${btn.target.value} is a number`);
-//                 break;
-//             case (btnArr.opr.includes(btn.target.value)):
-//                 console.log(`${btn.target.value} is an operator`);
-//                 break;
-//             case (btnArr.rst.includes(btn.target.value)):
-//                 console.log(`${btn.target.value} is a reset`);
-//                 break;
-//             case (btnArr.eql.includes(btn.target.value)):
-//                 console.log(`${btn.target.value} is an equal`);
-//                 break;
-//         }
-//     // })
-// } 
-
-// btnInput();
-
-
-// // List for button clicks and pass values
-// document.addEventListener('click', function buttonClick(e) {
-//     // Only pass values if elem clicked is button
-//     if (e.target.tagName === 'BUTTON') {
-//         // Handle first click
-//         if (result.innerText == finalTotal) {
-//             subTotal = e.target.value;
-//             result.innerText = subTotal;
-//         }  
-//         // Subsequent clicks
-//         else if (result.innerText != finalTotal) {
-//             // Don't display non-integer and non-operator values
-//             if ((e.target.value !== '=') && (e.target.value !== 'C') && (e.target.value !== 'CE')) {
-//                 // result.append(e.target.value);
-//             // Clear result with 'C' or 'CE' click
-//             } else if ((e.target.value === 'C') || (e.target.value === 'CE')) {
-//                 result.innerText = 0;
-//             // Run calculation on equal button click
-//             } else if (e.target.value === '=') {
-//                 result.innerText = (eval(result.innerText.valueOf()));
-//                 if (isNaN(result.innerText) === true) {
-//                     result.innerText = 'ERR';
-//                 }
-//             }
-//         } 
-//     }
-// })
